9f177ddb7cf505fa422aef3dd14978b49465c1dd
mrloulass <louislassegue@gmail.com>
2021-03-26 23:14:44 -0700

    Feature 3 done added Bootstrap

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import arrow_heart from './arrow_heart.png';
+import Card from 'react-bootstrap/Card'
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -18,16 +19,19 @@ class HornedBeast extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>{this.props.title}</h3>
-        <img onClick={this.imageClicked} src={this.props.image_url} alt={this.props.keyword} title={this.props.title} />
-        <p>{this.props.description} </p>
-        <p>Number of Horns: {this.props.horns}</p>
-        <div>
-          <img src={arrow_heart} alt="Arrow with Heart" />{this.state.numberOfLikesWithHearts}
-        </div>
-      </div>
-    )
+        <Card style={{ width: '18rem'}}>
+          <Card.Img variant="top" onClick={this.imageClicked} src={this.props.image_url} style={{ width: '100%'}} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.description}</Card.Text>
+            <Card.Text>Number of Horns: {this.props.horns}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <img src={arrow_heart} alt="Arrow with Heart" style={{ width: '50%'}} />
+          {this.state.numberOfLikesWithHearts}
+          </Card.Footer>
+        </Card>
+    );
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
-
+import CardColumns from 'react-bootstrap/CardColumns';
 import data from './data.json';
+import './Main.css';
 
 class main extends React.Component {
   render() {
-    let hornedBeastArray = data.map( animal => {
+    let hornedBeastArray = data.map(animal => {
       return <HornedBeast
         title = {animal.title}
         image_url = {animal.image_url}
         name = {animal.keyword}
         description = {animal.description}
         horns = {animal.horns}
-        />
+      />
     })
 
     return (
       <main>
-        {hornedBeastArray}
+        <CardColumns>
+          {hornedBeastArray}
+        </CardColumns>
       </main>
     );
   }
